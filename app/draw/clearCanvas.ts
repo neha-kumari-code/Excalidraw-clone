9d389c985cf1f9988f9b72709d237c988d48da34
Neha Kumari <neha-kumari-code>
2026-06-07 14:41:01 +0530

    panning zoom in zoom out

--- app/draw/clearCanvas.ts
+++ app/draw/clearCanvas.ts
@@ -1,18 +1,74 @@
 
+// import { ShapesType } from "./game";
+// import { ShapeType } from "@/generated/prisma/enums";
+
+// export async function clearCanvas(canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D,shapes:ShapesType[], selectedShape: ShapesType | null){
+   
+//     ctx.clearRect(0,0,canvas.width,canvas.height)
+//    shapes.forEach((s:{id:string,type:ShapeType,data:any}) => {
+//     ctx.beginPath();
+//     if(this.selectedShape?.id === shape.id){
+//     ctx.strokeStyle = "blue";
+//     ctx.lineWidth = 2;
+
+//     ctx.strokeRect(
+//         startX,
+//         startY,
+//         width,
+//         height
+//     );
+// }
+//     if(s.type=="RECT"){
+//         const d=s.data as any;
+//         ctx.rect(d.startX,d.startY,d.width,d.height)
+//         ctx.stroke();
+//     }
+//    });
+// }
+
+
 import { ShapesType } from "./game";
 import { ShapeType } from "@/generated/prisma/enums";
 
-export async function clearCanvas(canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D,shapes:ShapesType[]){
-   
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-   shapes.forEach((s:{id:string,type:ShapeType,data:any}) => {
-    ctx.beginPath();
-    if(s.type=="RECT"){
-        const d=s.data as any;
-        ctx.rect(d.startX,d.startY,d.width,d.height)
-        ctx.stroke();
-    }
-   });
+export function clearCanvas(
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+    shapes: ShapesType[],
+    selectedShape: ShapesType | null
+) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    shapes.forEach((s) => {
+        if (s.type === ShapeType.RECT) {
+            const d = s.data;
+
+            // Draw rectangle
+            ctx.beginPath();
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+
+            ctx.rect(
+                d.startX,
+                d.startY,
+                d.width,
+                d.height
+            );
+
+            ctx.stroke();
+
+            // Draw selection border
+            if (selectedShape?.id === s.id) {
+                ctx.beginPath();
+                ctx.strokeStyle = "blue";
+                ctx.lineWidth = 2;
+
+                ctx.strokeRect(
+                    d.startX,
+                    d.startY,
+                    d.width,
+                    d.height
+                );
+            }
+        }
+    });
 }
-
-
--- app/draw/game.ts
+++ app/draw/game.ts
@@ -17,10 +17,15 @@ export class Game{
     private cleanUp:(()=>void) | null=null
     private shapes:ShapesType[];
     private cameraX = 0;
-private cameraY = 0;
-private isPanning = false;
-private lastX = 0;
-private lastY = 0;
+    private cameraY = 0;
+    private isPanning = false;
+    private lastX = 0;
+    private lastY = 0;
+    private scale = 1;
+    private selectedShape: ShapesType | null = null;
+private isDraggingShape = false;
+private dragOffsetX = 0;
+private dragOffsetY = 0;
     constructor(tool:string,canvas:HTMLCanvasElement){
         this.canvas=canvas;
         this.ctx=canvas.getContext("2d")!
@@ -36,18 +41,25 @@ private lastY = 0;
          const {data}=await axios.get("/api/shapes")
         if(!data.success)return;
         this.shapes=data.shapes;
-        clearCanvas(this.canvas,this.ctx,this.shapes)
+        // clearCanvas(this.canvas,this.ctx,this.shapes,)
+        clearCanvas(
+    this.canvas,
+    this.ctx,
+    this.shapes,
+    this.selectedShape
+);
         this.mouseHandler();
     }
     mouseHandler(){
         this.canvas.addEventListener("mousedown",this.mouseDownHandler);
         this.canvas.addEventListener("mouseup",this.mouseUpHandler);
         this.canvas.addEventListener("mousemove",this.mouseMoveHandler)
-
+        this.canvas.addEventListener("wheel", this.wheelHandler);
         this.cleanUp= ()=>{
             this.canvas.removeEventListener("mousedown",this.mouseDownHandler);
             this.canvas.removeEventListener("mouseup",this.mouseUpHandler);
             this.canvas.removeEventListener("mousemove",this.mouseMoveHandler)
+            this.canvas.removeEventListener("wheel", this.wheelHandler);
         }
     }
 
@@ -58,12 +70,45 @@ private lastY = 0;
         this.lastY = e.clientY;
         return;
     }
+
+    const worldX =
+  (e.offsetX - this.cameraX) / this.scale;
+
+const worldY =
+  (e.offsetY - this.cameraY) / this.scale;
+
+  for(let i=this.shapes.length-1;i>=0;i--){
+    const shape=this.shapes[i];
+
+    if(shape.type===ShapeType.RECT){
+        const {startX,startY,width,height}=shape.data;
+
+        if(
+            worldX >= startX &&
+            worldX <= startX + width &&
+            worldY >= startY &&
+            worldY <= startY + height
+        ){
+             this.selectedShape = shape;
+        this.isDraggingShape = true;
+
+        this.dragOffsetX = worldX - startX;
+        this.dragOffsetY = worldY - startY;
+            break;
+        }
+    }
+}
+
          this.isDrawing=true;
         this.startX = e.offsetX - this.cameraX;
 this.startY = e.offsetY - this.cameraY;
     }
     mouseUpHandler=async(e:MouseEvent)=>{
-        
+        this.isDraggingShape = false;
+//         await axios.put("/api/shapes",{
+//     id:this.selectedShape?.id,
+//     data:this.selectedShape?.data
+// });
         this.isDrawing=false;
          this.isPanning = false;
         const width = (e.offsetX - this.cameraX) - this.startX;
@@ -87,9 +132,34 @@ const height = (e.offsetY - this.cameraY) - this.startY;
              ...shape
         })
     }
-    clearCanvas(this.canvas,this.ctx,this.shapes)
+    // clearCanvas(this.canvas,this.ctx,this.shapes)
+    clearCanvas(
+    this.canvas,
+    this.ctx,
+    this.shapes,
+    this.selectedShape
+);
     }
+
+
     mouseMoveHandler=(e:MouseEvent)=>{
+        if(this.isDraggingShape && this.selectedShape){
+
+    const worldX =
+      (e.offsetX - this.cameraX) / this.scale;
+
+    const worldY =
+      (e.offsetY - this.cameraY) / this.scale;
+
+    this.selectedShape.data.startX =
+      worldX - this.dragOffsetX;
+
+    this.selectedShape.data.startY =
+      worldY - this.dragOffsetY;
+
+    this.redraw();
+    return;
+}
           if (this.isPanning) {
         const dx = e.clientX - this.lastX;
         const dy = e.clientY - this.lastY;
@@ -105,7 +175,13 @@ const height = (e.offsetY - this.cameraY) - this.startY;
     }
         if(this.isDrawing){
              this.redraw();
-        clearCanvas(this.canvas,this.ctx,this.shapes)
+        // clearCanvas(this.canvas,this.ctx,this.shapes)
+        clearCanvas(
+    this.canvas,
+    this.ctx,
+    this.shapes,
+    this.selectedShape
+);
         if(this.tool==="rect"){
            
             const width=e.offsetX-this.startX;
@@ -118,14 +194,38 @@ const height = (e.offsetY - this.cameraY) - this.startY;
     }
     }
 
+  wheelHandler = (e: WheelEvent) => {
+    e.preventDefault();
+
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+
+    const worldX = (mouseX -this.cameraX) / this.scale;
+    const worldY = (mouseY-this.cameraY ) / this.scale;
+    const zoomFactor = e.deltaY > 0 ? 0.98 : 1.02;
+
+    this.scale *= zoomFactor;
+    
+
+    this.cameraX = mouseX - worldX * this.scale;
+    this.cameraY = mouseY - worldY * this.scale;
+
+    this.redraw();
+}
     redraw() {
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     // apply camera transform
-    this.ctx.setTransform(1, 0, 0, 1, this.cameraX, this.cameraY);
+    this.ctx.setTransform(this.scale, 0, 0, this.scale, this.cameraX, this.cameraY);
 
-    clearCanvas(this.canvas, this.ctx, this.shapes);
+    // clearCanvas(this.canvas, this.ctx, this.shapes);
+    clearCanvas(
+    this.canvas,
+    this.ctx,
+    this.shapes,
+    this.selectedShape
+);
 }
     setTool(tool:string){
         this.tool=tool
